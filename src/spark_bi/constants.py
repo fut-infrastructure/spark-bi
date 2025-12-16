972d4d1e78e433379cd23f0e7c1d1eaa5dec6192
Martin Bernstorff <martinbernstorff@gmail.com>
2025-12-16 13:41:49 +0100

    reports: episodesofcare

--- src/spark_bi/constants.py
+++ src/spark_bi/constants.py
@@ -14,6 +14,7 @@ class Extensions(enum.Enum):
     DK_CORE_REGIONAL_SUBDIVISION_CODES = (
         "http://hl7.dk/fhir/core/StructureDefinition/dk-core-RegionalSubDivisionCodes"
     )
+    EHEALTH_COLOCATION = "http://ehealth.sundhed.dk/cs/ehealth-system"
     EHEALTH_MUNICIPALITY_CODES = (
         "http://ehealth.sundhed.dk/fhir/StructureDefinition/ehealth-organization-municipalityCode"
     )
